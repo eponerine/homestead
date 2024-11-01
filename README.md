2adf489fec3f8e0c9ad1be606d265eb2b9ad8761
Ernie Costa <503974+eponerine@users.noreply.github.com>
2024-11-01 17:43:33 -0400

    Cpu temp (#1)
    * add CPU temp
    
    * Create config.json
    
    * Delete omfg.txt
    
    * Create README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a collection of scripts used around the homestead's various IoT devices to make life... more convenient :)
+
+Currently, the only major utlity is the Python script that collects Renogy solar controller data and shoves it into InfluxDB instance.
--- solar/save_solar_stats.py
+++ solar/save_solar_stats.py
@@ -30,6 +30,12 @@ battery_soc = float(controller.get_battery_state_of_charge())
 battery_temp = float(controller.get_battery_temperature())
 controller_temp = float(controller.get_controller_temperature())
 
+# CPU Temperature
+# Open the file and divide by 1000 for value in Centigrade
+file = open("/sys/class/thermal/thermal_zone0/temp")
+cpu_temp = (float(file.read()) / 1000)
+file.close()
+
 # Output to stdout
 print(current_time, end=',')
 print(solar_voltage, end=',')
@@ -41,11 +47,12 @@ print(load_power, end=',')
 print(battery_voltage, end=',')
 print(battery_soc, end=',')
 print(battery_temp, end=',')
-print(controller_temp)
+print(controller_temp, end=',')
+print(cpu_temp)
 
 # Connect to InfluxDB
 influxDB_client = influxdb_client.InfluxDBClient(url=influxDB_url,token=influxDB_token,org=influxDB_org)
 influxDB_writer = influxDB_client.write_api(write_options=SYNCHRONOUS)
 
 # Write to InfluxDB
-influxDB_writer.write(bucket=influxDB_bucket, record=[influxdb_client.Point("myMeasurement").tag("location", influxDB_location).field("solar_voltage", solar_voltage).field("solar_current", solar_current).field("solar_power", solar_power).field("load_voltage", load_voltage).field("load_current", load_current).field("load_power", load_power).field("battery_voltage",battery_voltage).field("battery_soc",battery_soc).field("battery_temp",battery_temp).field("controller_temp",controller_temp)])
+influxDB_writer.write(bucket=influxDB_bucket, record=[influxdb_client.Point("myMeasurement").tag("location", influxDB_location).field("solar_voltage", solar_voltage).field("solar_current", solar_current).field("solar_power", solar_power).field("load_voltage", load_voltage).field("load_current", load_current).field("load_power", load_power).field("battery_voltage",battery_voltage).field("battery_soc",battery_soc).field("battery_temp",battery_temp).field("controller_temp",controller_temp).field("cpu_temp",cpu_temp)])
